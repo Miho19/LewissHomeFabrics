d1628247f196e4ce1eedfc8c5546dbec923f842e
miho19 <miho19@hotmail.co.nz>
2026-01-21 23:15:04 +1300

    created Data project for database access

---
+++ Lewiss/Lewiss.Pricing.Data/Model/Customer.cs
@@ -0,0 +1,7 @@
+namespace Lewiss.Pricing.Data.Model;
+
+public class Customer
+{
+    public required Guid Id;
+        
+}
---
+++ Lewiss/Lewiss.Pricing.Data/Model/Worksheet.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Lewiss.Pricing.Data.Model;
+public class Worksheet
+{
+    [Key]
+    public required Guid Id {get; set;}
+    public required DateTimeOffset CreatedAt {get; set;}
+    public required Customer Customer {get; set;}
+
+    
+}
