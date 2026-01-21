cb1520936c950c866d857d29c5dc2538a93c9cb8
miho19 <miho19@hotmail.co.nz>
2026-01-21 21:31:39 +1300

    tests; Created PricingControllerTests; Removed boilerplate controllers;

---
+++ Lewiss/Lewiss.Pricing.Api.Tests/Systems/Controllers/PricingControllerTests.cs
@@ -0,0 +1,12 @@
+using Xunit.Abstractions;
+
+public class PricingControllerTests
+{
+    private readonly ITestOutputHelper _logger;
+    public PricingControllerTests(ITestOutputHelper logger)
+    {
+        _logger = logger;
+    }
+
+    
+}
