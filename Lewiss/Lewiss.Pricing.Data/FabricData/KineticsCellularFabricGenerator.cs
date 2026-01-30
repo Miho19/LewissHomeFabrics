0dd306e6afa384297f213d26b5f090c008cf911c
miho19 <miho19@hotmail.co.nz>
2026-01-30 22:05:48 +1300

    Removed ids from json for kinetics cellular fabric data json to make it easier to add new fabrics

--- Lewiss/Lewiss.Pricing.Data/FabricData/KineticsCellularFabricGenerator.cs
+++ Lewiss/Lewiss.Pricing.Data/FabricData/KineticsCellularFabricGenerator.cs
@@ -1,4 +1,18 @@
+using Lewiss.Pricing.Data.FabricData;
+using Lewiss.Pricing.Data.Model.Fabric;
+
 public static class KineticsCellularFabricGenerator
 {
+    public static string KineticsCellularJSONFabricDataFileName { get; } = "KineticsCellularFabricData.json";
+    public static async Task<List<KineticsCellularFabric>> FabricListAsync(CancellationToken cancellationToken = default)
+    {
+        var jsonString = FabricDataUtility.GetFileData(KineticsCellularJSONFabricDataFileName, cancellationToken);
+        if (jsonString is null)
+        {
+            throw new Exception("Failed to retrieve Kinetics Cellular Fabric JSON file");
+        }
 
+
+        return [];
+    }
 }
