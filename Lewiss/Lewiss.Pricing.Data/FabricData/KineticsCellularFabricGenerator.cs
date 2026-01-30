b521a110b8e9cfa2b662b29efb374fa4118a0739
miho19 <miho19@hotmail.co.nz>
2026-01-30 22:11:23 +1300

    Deserilise the json data into Fabric Data Structure, ready to use select to produce fabric data model

--- Lewiss/Lewiss.Pricing.Data/FabricData/KineticsCellularFabricGenerator.cs
+++ Lewiss/Lewiss.Pricing.Data/FabricData/KineticsCellularFabricGenerator.cs
@@ -1,17 +1,35 @@
+using System.Text.Json;
 using Lewiss.Pricing.Data.FabricData;
 using Lewiss.Pricing.Data.Model.Fabric;
 
 public static class KineticsCellularFabricGenerator
 {
+
+    private record KineticsCellularFabricDataJSONStructure()
+    {
+        public required string Code;
+        public required string Colour;
+        public required string Opacity;
+        public required int Multiplier;
+
+    }
+
     public static string KineticsCellularJSONFabricDataFileName { get; } = "KineticsCellularFabricData.json";
     public static async Task<List<KineticsCellularFabric>> FabricListAsync(CancellationToken cancellationToken = default)
     {
-        var jsonString = FabricDataUtility.GetFileData(KineticsCellularJSONFabricDataFileName, cancellationToken);
+        var jsonString = await FabricDataUtility.GetFileData(KineticsCellularJSONFabricDataFileName, cancellationToken);
         if (jsonString is null)
         {
             throw new Exception("Failed to retrieve Kinetics Cellular Fabric JSON file");
         }
 
+        var jsonData = JsonSerializer.Deserialize<List<KineticsCellularFabricDataJSONStructure>>(jsonString);
+        if (jsonData is null)
+        {
+            throw new Exception("Failed to deserialise Kinetics Cellular Fabric data");
+        }
+
+        jsonData.Select(f => );
 
         return [];
     }
